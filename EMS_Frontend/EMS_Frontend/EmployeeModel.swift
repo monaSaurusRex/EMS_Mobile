e8640bce3b4097b8c02e915e04df3a2164772ad0
Monique <monqiquenagel24.mn@gmail.com>
2023-05-25 09:27:35 +0200

    Created employee model

---
+++ EMS_Frontend/EMS_Frontend/EmployeeModel.swift
@@ -0,0 +1,20 @@
+//
+//  EmployeeModel.swift
+//  EMS_Frontend
+//
+//  Created by DA MAC M1 158 on 2023/05/25.
+//
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let employee = try? JSONDecoder().decode(Employee.self, from: jsonData)
+
+import Foundation
+
+// MARK: - EmployeeElement
+struct Employee: Codable {
+    let id: Int
+    let firstName, lastName, email, contactNo: String
+    let deleted: Bool
+}
