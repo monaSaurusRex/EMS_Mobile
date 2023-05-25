7f8f8aad1a524abd84538054a55814dda79b3a83
Monique <monqiquenagel24.mn@gmail.com>
2023-05-25 07:55:44 +0200

    Added employee info cell to table view.

---
+++ EMS_Frontend/EMS_Frontend/EmployeeTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EmployeeTableViewCell.swift
+//  EMS_Frontend
+//
+//  Created by DA MAC M1 158 on 2023/05/25.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var employeeNum: UILabel!
+    @IBOutlet weak var empFirstName: UILabel!
+    @IBOutlet weak var empLastName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ EMS_Frontend/EMS_Frontend/EmployeeTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EmployeeTableViewController.swift
+//  EMS_Frontend
+//
+//  Created by DA MAC M1 158 on 2023/05/25.
+//
+
+import UIKit
+
+class EmployeeTableViewController: UIViewController {
+
+    @IBOutlet weak var employeeTV: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
